581e4990c57ebb9e23605cc19329139fbfd9d92b
Ostik <117195748+Ostik1220@users.noreply.github.com>
2025-10-19 18:58:22 +0300

    counter class component

--- src/App.js
+++ src/App.js
@@ -3,14 +3,21 @@ import { PageBoard } from './PageBoard/PageBoard';
 import upcomingEvents from './upcoming-events.json';
 import { GlobalStyle } from './GlobalStyle';
 import { Title } from './Title/Title';  
+import Counter from './Counter/Counter'; 
+
 
 function App() {
   console.log(upcomingEvents);
   return (
     <div className="App">
+      {/* lesson 1-6 
         <Title />
       <PageBoard events={upcomingEvents}/>
         <GlobalStyle />
+        */}
+        
+        {/* lesson 7 */}
+        <Counter />
     </div>
   );
 }
---
+++ src/Counter/Counter.jsx
@@ -0,0 +1,46 @@
+import { Component } from 'react';
+
+class Counter extends Component {
+    // constructor () {
+    //     super();
+    //     this.value=0;
+    // } в реакті не обовязково писати конструктор бо він створюється автоматично
+
+    state = {
+        test: true,
+        value: 0,
+        text: 'hello world'
+    };
+
+    plus = () => {
+        // this.state.value = this.state.value + 1; неправильно так робити
+        this.setState({
+            value: 5
+        })
+    }  
+
+    minus = () => {
+        // this.state.value = this.state.value - 1; неправильно так робити
+        // this.setState({
+        //     value: 0
+        // }) передаємо обєкт якщо треба змінити старе значення на нове
+        // якщо ми старе значення хочем змінити і записати як нове то передаємо в сетстейт колбек функцію
+        this.setState((prevState) =>{
+            return { value: prevState.value - 1 }   
+        })
+    }   
+
+
+
+    render() {
+        return (
+            <div>
+                <button onClick={this.plus}>+</button>
+                <h3>{this.state.value}</h3>
+                <button onClick={this.minus}>-</button>
+            </div>
+        );
+    }
+}
+
+export default Counter;
